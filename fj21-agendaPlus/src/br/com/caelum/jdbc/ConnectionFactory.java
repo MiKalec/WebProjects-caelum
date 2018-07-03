1f1ea868b7395b45623c39a33bc2025c076f7a3c
MiKalec <msmikaelis@gmail.com>
2018-07-03 13:57:43 -0300

    first commit

---
+++ fj21-agendaPlus/src/br/com/caelum/jdbc/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package br.com.caelum.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+    public Connection getConnection(){
+    	try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+        try{
+            return DriverManager.getConnection("jdbc:mysql://localhost/jdbc", "root", "Mik@k@1234");
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
